1e171bd4665106cd706b573891dfdae17886a9e2
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-22 23:10:57 +0800

    P4.根號 (Sqrt)

---
+++ k516.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int K;
+    while(cin >> K){                    //圖形長
+        int K2=4*K+(K-K/2-1)-2;        //圖形寬
+        char picture[K][K2];
+        for(int i=0;i<K;i++){
+            for(int j=0;j<K2;j++){
+                picture[i][j]=' ';       //陣列初始化
+            }
+        }
+
+        int nowx=0,nowy=K/2;
+
+        for(int i=0;i<K;i++){
+            picture[nowy][nowx]='*';     //短橫線
+            nowx++;
+        }
+      
+         nowx--;
+        while(nowy<K){
+            picture[nowy][nowx]='*';      //左上右下的斜線
+            nowy++;
+            nowx++;
+        }        
+      
+         nowx-=2;  
+        while(nowy>-1){                    //右上左下的斜線
+            nowy--;
+            nowx++;
+            picture[nowy][nowx]='*';
+
+        }
+       nowy++;
+       while(nowx<K2&&nowy==0){            //長橫線
+            picture[nowy][nowx]='*';
+            nowx++;
+        }
+              
+       for(int i=0;i<K;i++){
+            for(int j=0;j<K2;j++){
+                cout << picture[i][j];
+            }
+            cout << endl;
+       }
+    }
+
+return 0;
+}
