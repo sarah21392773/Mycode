7d015b146a96735c2a4e0324b17d47f1793ca409
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-22 23:18:08 +0800

    p&q的邂逅

---
+++ i213.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int thenum=0;
+        string a;
+        cin >> a;
+        stack<char>s;
+        for(int j=0;j<a.size();j++){
+            if(a[j]!='.'){
+                if(!s.empty()){
+                    if(s.top()=='p' && a[j]=='q'){
+                        s.pop();
+                        thenum++;
+                    }
+                    else{
+                        s.push(a[j]);
+                    }
+                }
+                else{
+                    s.push(a[j]);
+                }
+            }
+        }
+        cout << thenum <<endl;
+    }
+return 0;
+}
