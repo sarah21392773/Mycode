90e5637232ed82f18b303fb3d320e81ec8b4207c
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-22 23:22:05 +0800

    Create i213.cpp
    stack 練習

---
+++ i213.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N,k;
+    cin >> N;
+    stack<int>v;
+    for(int i=0;i<N;i++){
+        cin >> k;
+        if(k==1){
+            int x;
+            cin >> x;
+            v.push(x);
+        }
+        else if(k==2){
+            if(!v.empty()) cout << v.top() <<endl;
+            else cout << -1 <<endl;
+        }
+        else{
+            if(!v.empty()) v.pop();
+        }
+    }
+return 0;
+}
