bfdd9efa1e2342f1351ca903380eba812fd26b47
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-22 22:32:59 +0800

    n360.cpp
    搶21

--- The21Game.cpp
+++ The21Game.cpp
@@ -3,18 +3,15 @@ using namespace std;
 int main(){
     int T,R;
     while(cin >> T >> R){
-        int aa=T-(1+R)*(T/(1+R));
-        cout << aa << endl;
-        //cout << aa/R << endl;
-        //cout << aa/R%2 << endl;
-        if(aa==0){
+        int aa=T-(1+R)*(T/(1+R));     //搶到aa就能獲勝
+        if(aa==0){                    //aa等於0時，就加上R+1
             aa+=R+1;
         }
-        if(aa<=R){
+        if(aa<=R){                    //若aa小於報數範圍，則先手勝
             cout << "1" << endl;
         }
         else{
-            cout << "0" << endl;
+            cout << "0" << endl;       //反之，後者勝
         }
     }
 return 0;
