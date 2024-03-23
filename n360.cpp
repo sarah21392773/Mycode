b2732642c8fdf40ef41a5b684a998ffa85f44357
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-23 08:42:35 +0800

    搶21 (The 21 Game)

--- n360.cpp
+++ n360.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     int T,R;
+    
     while(cin >> T >> R){
         int aa=T-(1+R)*(T/(1+R));     //搶到aa就能獲勝
         if(aa==0){                    //aa等於0時，就加上R+1
@@ -14,5 +15,6 @@ int main(){
             cout << "0" << endl;       //若aa大於報數範圍，則後者勝
         }
     }
+    
 return 0;
 }
