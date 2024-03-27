a5116f2a1561fe4eb8d7705d833bc9e7492d1620
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-27 21:05:19 +0800

    同樂會 (Party)

--- toi/f337.cpp
+++ toi/f337.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 
-    int N,M,piece;
+    int N,M,piece;         //N為人數，M為Pizza數，piece為Pizza可被切成的片數
     cin >> N >> M;
-    piece = 8*M;
-    if(piece<2*N){
+    piece = 8*M;           //一塊Pizza可被切成8片
+    if(piece<2*N){         //每一人至少吃2片
         cout << "Not enough";
     }
-    else if(piece>3*N){
+    else if(piece>3*N){    //每一人最多吃3片
         cout << "Too much";
     }
     else{
