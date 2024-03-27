4d2358131a76e6ad4577688ebbaf298b2ac1ea18
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-27 20:55:06 +0800

    英文縮寫 (Abbreviation)

--- toi/f515.cpp
+++ toi/f515.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
 
     string a;
-    while(cin >> a){
+    while(cin >> a){                   //a可能大寫或小寫
         char c[a.size()];
         string b="";
         for(int i=0;i<a.size();i++){
-            c[i]=toupper(a[i]);
-            b += c[i];
+            c[i]=toupper(a[i]);        //將字串全部轉成大寫
+            b += c[i];                 //儲存於b字串中(大寫)
         }
-        if(b=="FOR"){
+        if(b=="FOR"){                  //特殊縮寫規則
             cout << "4";
         }
         else if(b=="TO"){
@@ -23,7 +23,7 @@ int main(){
             cout << "u";
         }
         else{
-            char t = toupper(b[0]);
+            char t = toupper(b[0]);    //若不符合特殊縮寫規則，就輸出單字的大寫開頭
             cout << t;
         }
     }
