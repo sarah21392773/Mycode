363e1d15194dd5ea429d02c8bad8f080d42d271e
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-27 21:28:02 +0800

    虛擬寵物大師 (Master)

--- toi/e622.cpp
+++ toi/e622.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int n,S,ThePet=0,MaxCP=0;
+    int n,S,ThePet=0,MaxCP=0;    //n為寵物數量，S為擁有沙子數，ThePet為要捕捉的寵物，MaxCP為最大CP值
     cin >> n >> S ;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){        //比較每一隻寵物
         int CP=0,IV=0;
         cin >> CP >> IV;
         if(IV<30){
@@ -16,8 +16,8 @@ int main() {
         else{
             CP+=S/1000*100;
         }
-        if(CP>MaxCP){
-            MaxCP=CP;
+        if(CP>MaxCP){            //若CP值大於最大CP值
+            MaxCP=CP;            //則更新MaxCP及ThePet
             ThePet=i+1;
         }
     }
