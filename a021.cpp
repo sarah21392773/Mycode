26317c48f2e322ec0e6665e79066ba668a16e82d
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-03-31 09:25:52 +0800

    unfinished

--- a021.cpp
+++ a021.cpp
@@ -2,28 +2,74 @@
 using namespace std;
 
 int main(){
-    string a,b,c;
+    string a,b;
+    char c;
     cin >> a >> c >> b;
-    int ans[500]={0};
+//    cout << a << " " << b << " " << c <<endl;
+    int ans[10]={0},i,A[10],B[10];
+//    cout << a.size() << " " << b.size() << endl;
+    for(int i=0;i<a.size();i++){
+        A[a.size()-i-1]=a[i]-'0';
+    }
+    for(int i=0;i<b.size();i++){
+        B[b.size()-i-1]=b[i]-'0';
+    }
+/*
+    for(int i=0;i<a.size();i++){
+        cout << A[i] ;
+    }
+    cout << endl;
+    for(int i=0;i<b.size();i++){
+        cout << B[i];
+    }
+    cout << endl;
+*/
+
+    if(a.size()>b.size()){
+        i=a.size();
+    }
+    else{
+        i=b.size();
+    }
+//    cout << i << endl;
     if(c=='+'){
-        for(int i=0;i<a.size()||i<b.size();i++){
-            ans[i]=a[i]+b[i];
+        for(int j=0;j<i;j++){
+            ans[j]+=A[j]+B[j];
+//            cout << ans[j] << " " ;
+            if(ans[j]>=10){
+                ans[j+1]+=ans[j]/10;
+                ans[j]%=10;
+            }
+ //           cout << ans[j] << " " ;
         }
     }
     else if(c=='-'){
         for(int i=0;i<a.size()||i<b.size();i++){
             ans[i]=a[i]-b[i];
-        }        
+            if(a[i]<b[i]){
+                ans[i]+=10;
+                ans[i+1]--;
+            }
+        }
     }
     else if(c=='*'){
         for(int i=0;i<a.size()||i<b.size();i++){
             ans[i]=a[i]*b[i];
-        }        
+            if(ans[i]>=10){
+                ans[i+1]+=ans[i]/10;
+                ans[i]%=10;
+            }
+        }
     }
+/*
     else{
         for(int i=0;i<a.size()||i<b.size();i++){
             ans[i]=a[i]/b[i];
-        }        
+        }
+    }
+*/
+    for(int i=10;i>=0;i--){
+        cout << ans[i];
     }
 return 0;
 }
