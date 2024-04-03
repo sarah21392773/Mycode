e6a98d6ced34a3bfb3740d9bdd8cfb4fe43dc9c6
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-03 13:42:49 +0800

    判斷質數-商競103

---
+++ b513.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=0;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int x=0,j=2;
+        bool a=true;
+        cin >> x;
+        while(j!=x){
+            if(x%j==0){
+                a=false;
+                break;
+            }
+            j++;
+        }
+        if(a==true){
+            cout << "Y" << endl;
+        }
+        else{
+            cout << "N" << endl;
+        }
+    }
+    return 0;
+}
