4a4f89709db593224681472d57531aee8f4ef4a8
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-03 21:56:37 +0800

    二分搜尋法 unfinshed

---
+++ d732.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    int arr[n],x;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<k;i++){
+        cin >> x;
+    }
+return 0;
+}
