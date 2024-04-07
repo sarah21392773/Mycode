7dcc4c3db000e14799471cb85e26ac602e56722e
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-07 20:20:14 +0800

    Flip Sort

--- other/a539.cpp
+++ other/a539.cpp
@@ -1,21 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    while(cin >> n){
-        int num[n],time=0;
-        for(int i=0;i<n;i++){
-            cin >> num[i];
-        }
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(num[i] > num[j]){
-                    time++;
-                    swap(num[i],num[j]);
-                }
+
+int bubble(int* arr,int n){
+    int time=0;     //最少須交換time次
+    for(int i=0;i<n;i++){   //執行n回合
+        for(int j=0;j<n-i-1;j++){   //每執行i回合，就會有i數被排序完成
+            if(arr[j]>arr[j+1]){    //若前數>後數
+                swap(arr[j],arr[j+1]);  //就交換
+                time++;     
             }
         }
-        cout << "Minimum exchange operations : " << time << endl;
+    }
+    return time;    //排序完成後，回傳最小交換次數
+}
+
+int main(){
+    int n=0;        //有n數要做排列
+    while(cin >> n){
+        int arr[n];
+        for(int i=0;i<n;i++) cin >> arr[i];
+        cout << "Minimum exchange operations : " << bubble(arr,n) << endl;  //用自訂函式算出最小交換次數
     }
 return 0;
 }
+
