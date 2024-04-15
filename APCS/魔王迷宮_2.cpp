735628300e71639bbb12ccf64095b608f75a3f60
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-15 17:30:50 +0800

    Update 魔王迷宮_2.cpp

--- APCS/魔王迷宮_2.cpp
+++ APCS/魔王迷宮_2.cpp
@@ -1,28 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+struct TheBoss{
+    int r=0,c=0,s=0,t=0;
+    int is_live=1;
+};
+
 int main(){
     int n,m,k;
     cin >> n >> m >> k;
-    int filed[n+2][m+2],bomb[n+2][m+2];
-    for(int i=0;i<n+2;i++){
-        for(int j=0;j<m+2;j++){
-            filed[i][j]=0;
-            bomb[i][j]=0;
-        }
-    }
-    int r[4]={0},c[4]={0},s[4]={0},t[4]={0};
+    int boss_num=k
+    vector<int>bomb[3]={0};
+
+    TheBoss boss[4];
     for(int i=0;i<k;i++){
-        cin >> r[i] >> c[i] >> s[i] >> t[i];
-        filed[r[i]+1][c[i]+1]=3;  //魔王
+        cin >> boss[i].r >> boss[i].c >> boss[i].s >> boss[i].t;
     }
-    while(k>0){
+    while(boss_num>0){
         for(int i=0;i<k;i++){
-            if(r[i]+1+s[i]>m+1||r[i]+1+s[i]<1||c[i]+1+t[i]>n+1||c[i]+1+t[i]>n+1){}
+            if(boss[i].is_live==1){
+                if(boss[i].r+boss[i].s>=m||boss[i].r+boss[i].s<0||boss[i].c+boss[i].t>=n||boss[i].c+boss[i].t<0){
+                    boss_num--;
+                    boss[i].is_live=0;
+                }
+                else{
+                    bomb[0].push(boss[i].r);
+                    bomb[1].push(boss[i].c);
+                    bomb[2].push(1);
+                    boss[i].r+=boss[i].s;
+                    boss[i].c+=boss[i].t;
+                }
+            }
         }
+        if()
     }
 /*
-    for(int i=0;i<n+2;i++){
-        for(int j=0;j<m+2;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             cout << filed[i][j] << " ";
         }
         cout << endl;
