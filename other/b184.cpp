947b1b3341421ba8fadd1599b7b64c0bd624d1f5
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-17 15:32:59 +0800

    b184. 5. 裝貨櫃問題 unfished

---
+++ other/b184.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 105;
+const int MAXW = 105;
+int w[MAXN], p[MAXN];
+int K[MAXN][MAXW];
+
+int main(){
+
+	int n;
+	while(cin >> n){
+		for(int i = 1; i <= n; i++)
+			cin >> w[i] >> p[i];
+		for(int i = 0; i <= n ; i++){
+			for(int j = 0; j <= n ; j++){
+				if(i == 0 || j == 0)  K[i][j] = 0 ;
+				else if(w[i] > j)  K[i][j] = 0;
+				else  K[i][j] = max(p[i]+K[i-1][j-w[i]],K[i-1][j]);
+			}
+		}
+
+		for(int i = 0; i <= n ; i++){
+			for(int j = 0; j <= n ; j++){
+				cout << K[i][j] << " ";
+			}
+			cout << endl;
+		}
+
+
+//		cout << K[][] << endl;
+	}
+
+return 0;
+}
