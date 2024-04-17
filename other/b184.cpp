97510e2c355fe728d7cad67e22deaaa2c36ae066
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-17 15:47:38 +0800

    Update b184.cpp

--- other/b184.cpp
+++ other/b184.cpp
@@ -9,25 +9,31 @@ int main(){
 
 	int n;
 	while(cin >> n){
+
 		for(int i = 1; i <= n; i++)
 			cin >> w[i] >> p[i];
+
+
 		for(int i = 0; i <= n ; i++){
-			for(int j = 0; j <= n ; j++){
+			for(int j = 0; j <= 100 ; j++){
 				if(i == 0 || j == 0)  K[i][j] = 0 ;
 				else if(w[i] > j)  K[i][j] = 0;
 				else  K[i][j] = max(p[i]+K[i-1][j-w[i]],K[i-1][j]);
 			}
 		}
 
+
+/*
 		for(int i = 0; i <= n ; i++){
-			for(int j = 0; j <= n ; j++){
+			for(int j = 0; j <= 100 ; j++){
 				cout << K[i][j] << " ";
 			}
 			cout << endl;
 		}
+*/
 
 
-//		cout << K[][] << endl;
+		cout << K[n][100] << endl;
 	}
 
 return 0;
