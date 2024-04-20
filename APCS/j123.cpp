cf353a2b476a362a2d1ffecc586b47f6497ada22
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-20 08:20:14 +0800

    Create j123.cpp

---
+++ APCS/j123.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int check(char t,int y){
+    if(t=='A'){
+    
+    }
+    if(t=='B'){
+    }
+    if(t=='C'){
+    
+    }
+    if(t=='D'){
+    
+    }
+    if(t=='E'){
+    
+    }
+}
+
+int main()
+{
+    int R,C,n;
+    cin >> R >> C >> n;
+    int arr[R][C];
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            arr[i][j]=0;
+        }
+    }
+    for(int i=0;i<n;i++){
+        char t;
+        int y;
+        cin>>t>>y;
+    }
+    return 0;
+}
