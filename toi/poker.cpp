19fb539f5dec49d6e539254bac18eef73ca43233
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-04-29 17:22:47 +0800

    Update poker.cpp

--- toi/poker.cpp
+++ toi/poker.cpp
@@ -4,20 +4,25 @@ using namespace std;
 int main(){
     int n;
     while(cin >> n){
-        int poker[52]={0},p=500;
+        int poker[52]={0},p=500,big=0;
         for(int i=0;i<n;i++){
             int card;
             cin >> card;
             poker[card-1]++;
-            p=min(p,poker[card-1]);
+            if(poker[card-1]<p&&poker[card-1]*13<=n){
+                p=poker[card-1];
+            }
+            big=max(big,poker[card-1]);
 //            cout << p << endl;
         }
+//        cout << big << endl;
+        if (p==500) p=0;
         int more=0;
         for(int i=0;i<52;i++){
             if(poker[i]>p){
-                cout << poker[i] << " " << p << endl;
-                more+=poker[i]-p;
+//                cout << poker[i] << " " << p << endl;
             }
+            more+=big-poker[i];
         }
         cout << p << " " << more << endl;
     }
