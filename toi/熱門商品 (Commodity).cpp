91795e35bcd14b7782cd9735da7f2e1de5cee4b5
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-05-13 17:03:53 +0800

    Update 熱門商品 (Commodity).cpp

--- toi/熱門商品 (Commodity).cpp
+++ toi/熱門商品 (Commodity).cpp
@@ -1,26 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int k,n;
-    while(cin >> k >> n){
-        int a[k],num[n],populer,like=0;
-        for(int i=0;i<k;i++){
-            cin >> a[i];
+    int K,N;
+    while(cin >> K >> N){
+        int popularity[N]={0};
+        pair<int,int> ans={-1,1000};
+        int R[K]={0},M[N]={0};
+        for(int i=0;i<K;i++) cin >> R[i];
+        for(int i=0;i<N;i++) cin >> M[i];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M[i];j++){
+                int goods;
+                cin >> goods;
+                for(int k=0;k<K;k++){
+                    if(goods==R[k]) popularity[i]++;
+                }
+            }
+            if(popularity[i]<ans.second){
+                ans.second=popularity[i];
+                ans.first=i+1;
+            }
+//            cout << "shop" << i << ":" << popularity[i] << endl;
         }
-        for(int i=0;i<k;i++){
-            like=max(count(a,a[i]),like);
-            if(count(a,a[i])>like) populer=i+1;
-        }
-        for(int i=0;i<n;i++){
-            cin >> num[i];
-        }
-        int a,shop;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<num[i];j++){
-                
+        pair<int,int> a={-1,0};
+        for(int i=0;i<K;i++){
+            int num=0;
+            for(int j=0;j<K;j++){
+                if(R[i]==R[j]) num++;
+            }
+            if(num>a.second){
+                a.first=R[i];
+                a.second=num;
             }
         }
+        cout << a.first << " " << ans.first << endl;
     }
 return 0;
 }
