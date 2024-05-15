9b3ac6eb000edff4bfc0a948587fc117fb6863e1
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-05-15 21:20:52 +0800

    Update 熱門商品 (Commodity).cpp

--- toi/熱門商品 (Commodity).cpp
+++ toi/熱門商品 (Commodity).cpp
@@ -3,31 +3,30 @@ using namespace std;
 int main(){
     int K,N;
     while(cin >> K >> N){
-        int popularity[N]={0};
-        pair<int,int> ans={-1,1000};
+        pair<int,int> shop={-1,1000},product={-1,0};
         int R[K]={0},M[N]={0};
-        for(int i=0;i<K;i++) cin >> R[i];
+        for(int i=0;i<K;i++){
+            cin >> R[i];
+            int num=count(R,R+K,R[i]);
+            if(num>product.second){
+                product.first=R[i];
+                product.second=num;
+            }
+        }
         for(int i=0;i<N;i++) cin >> M[i];
         for(int i=0;i<N;i++){
+            int popularity=0;
             for(int j=0;j<M[i];j++){
                 int goods;
                 cin >> goods;
-                popularity[i]+=count(R,R + K,goods);
+                popularity+=count(R , R + K , goods);
             }
-            if(popularity[i]<ans.second){
-                ans.second=popularity[i];
-                ans.first=i+1;
-            }
-        }
-        pair<int,int> a={-1,0};
-        for(int i=0;i<K;i++){
-            int num=count(R,R+K,R[i]);
-            if(num>a.second){
-                a.first=R[i];
-                a.second=num;
+            if(popularity<shop.second){
+                shop.second=popularity;
+                shop.first=i+1;
             }
         }
-        cout << a.first << " " << ans.first << endl;
+        cout << product.first << " " << shop.first << endl;
     }
 return 0;
 }
