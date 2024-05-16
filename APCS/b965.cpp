aa49a5ecdec65681dae71f455c608b866203aa6c
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-05-16 22:14:31 +0800

    Update b965.cpp

--- APCS/b965.cpp
+++ APCS/b965.cpp
@@ -4,61 +4,107 @@ using namespace std;
 int main(){
     int R,C,M;
     cin >> R >> C >> M;
-    int A[R][C],B[C][R],arr=0;
-    for(int i=0;i<R;i++)
-        for(int j=0;j<C;j++)
-            cin >> A[i][j];
-    for(int i=0;i<M;i++){
-        int x;
-        cin >> x;
-        if(x==1){
-            if(arr==0) int tem[R][C];
-            else int tem[C][R];
-            for(int j=0;j<R;j++){
-                for(int k=0;k<C;k++){
-                    if(arr==0)
-                        tem[R-j-1][k]=A[j][k];
-                    else
-                        tem[R-j-1][k]=B[j][k];
-                }
-            }
-            if(arr==0)
-                A=tem;
-            else
-                B=tem;
-        }
-        else{
-            for(int j=0;j<R;j++){
-                for(int k=0;k<C;k++){
-                    if(arr=0){
-                        B[k][j]=A[j][k];
-                    }
-                    else(arr=1){
-                        A[k][j]=B[j][k];
-                    }
-                }
-            }
-            if(arr==0) arr=1;
-            else arr=0;
+    int a[R][C],b[C][R];
+    char a_or_b='a';
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            cin >> a[i][j];
         }
     }
-    if(arr==0){
-        cout << R << " " << C << endl;
+
+    for(int i=0;i<M;i++){
+        int movement;
+        cin >> movement;
+        if(movement==1){
+            if(a_or_b=='a'){
+                int tem[R][C];
+                for(int j=0;j<R;j++){
+                    for(int k=0;k<C;k++){
+                        tem[k][j]=a[C-k-1][j];
+                    }
+                }
+                for(int j=0;j<R;j++){
+                    for(int k=0;k<C;k++){
+                        a[k][j]=tem[k][j];
+                    }
+                }
+            }
+            else{
+                int tem[C][R];
+                for(int j=0;j<C;j++){
+                    for(int k=0;k<R;k++){
+                        tem[k][j]=b[R-k-1][j];
+                    }
+                }
+                for(int j=0;j<R;j++){
+                    for(int k=0;k<C;k++){
+                        b[j][i]=tem[j][i];
+                    }
+                }
+            }
+        }
+        else{
+            if(a_or_b=='a'){
+                a_or_b='b';
+                for(int j=0;j<R;j++){
+                    for(int k=0;k<C;k++){
+                        b[k][j]=a[j][C-k-1];
+                    }
+                }
+            }
+            else{
+                a_or_b='a';
+                for(int j=0;j<C;j++){
+                    for(int k=0;k<R;k++){
+                        a[j][k]=b[R-k-1][j];
+                    }
+                }
+            }
+
+        }
+    if(a_or_b=='a'){
         for(int i=0;i<R;i++){
             for(int j=0;j<C;j++){
-                cout << A[i][j] << " ";
+                cout << a[i][j] << " ";
             }
             cout << endl;
         }
     }
     else{
-        cout << C << " " << R << endl;
         for(int i=0;i<C;i++){
             for(int j=0;j<R;j++){
-                cout << B[i][j] << " ";
+                cout << b[i][j] << " ";
             }
             cout << endl;
         }
     }
+    cout << endl;
+
+    }
+    if(a_or_b=='a'){
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else{
+        for(int i=0;i<C;i++){
+            for(int j=0;j<R;j++){
+                cout << b[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+/*
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+*/
 return 0;
 }
