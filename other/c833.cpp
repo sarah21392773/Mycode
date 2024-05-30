74137e709e8dff14d7bfb281c49ecdb1ea7d3602
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-05-30 17:01:32 +0800

    Update c833.cpp

--- other/c833.cpp
+++ other/c833.cpp
@@ -1,19 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int n,m;
-    cin >> n >> m;
-    char place;
-    map<int,bool> x,y;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin >> place;
-            if(place=='#'){
+    int N,M;
+    cin >> N >> M;
+    map <int,bool> x,y;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            char a;
+            cin >> a;
+            if(a=='#'){
                 x[j]=true;
                 y[i]=true;
             }
         }
+    }
+/*
+    for (const auto& s : x) {
+        std::cout << "x: " << s.first <<"\n";
+    }
+    for (const auto& s : y) {
+        std::cout << "y: " << s.first <<"\n";
+    }
+*/
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(x[j]==true||y[i]==true){
+                cout << '#';
+            }
+            else{
+                cout << 'X';
+            }
+        }
+        cout << endl;
     }
 return 0;
 }
