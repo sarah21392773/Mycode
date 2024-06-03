409a41c113fc445932ae4081de7b9d894d5eb221
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-06-03 21:39:48 +0800

    Update m931.cpp

--- APCS/m931.cpp
+++ APCS/m931.cpp
@@ -14,10 +14,10 @@ int main(){
         cin >> player.attack >> player.protection;
         player.ability=pow(player.attack,2)+pow(player.protection,2);
         if(player.ability>First.ability){
-                if(Second.ability==0){
-                    Second.ability=player.ability;
-                    Second.attack=player.attack;
-                    Second.protection=player.protection;
+                if(First.ability>Second.ability){
+                    Second.ability=First.ability;
+                    Second.attack=First.attack;
+                    Second.protection=First.protection;
                 }
                 First.ability=player.ability;
                 First.attack=player.attack;
