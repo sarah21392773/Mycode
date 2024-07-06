eb165c4ae32bc6a28b09c536960084de11f0372e
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-07-06 09:03:53 +0800

    Update o078.cpp

--- APCS/o078.cpp
+++ APCS/o078.cpp
@@ -1,25 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string K,S,ans;
+int L;
+map<string,int> is_exist;
+
+void DFS(int idx){
+    //如果ans長度達L
+    if(idx>=L){
+        //如果出現在is_exist中，就不是答案
+        if(is_exist[ans]>=1) return;
+        //否則是答案
+        cout << ans << endl;
+        exit(0);
+    }
+    if (idx >= ans.size()) return;
+    //否則ans長度未達L，就繼續枚舉
+    for(int i=0;i<S.size();i++){
+        ans[idx] = S[i];
+        // 繼續 DFS，因為選了一個字母所以 idx 和 cnt 都要 + 1
+        DFS(idx+1);
+    }
+}
+
 int main(){
-    string K,S;
-    int L;
+
+    cin.sync_with_stdio(0);
+    cin.tie(0);
     cin >> K >> L >> S;
-    
+
     //看S中出現字串
-    map<string,int> is_exist;
     for(int i=0;i<S.length()-L+1;i++){
-        string a;
-        for(int j=i;j<i+L;j++){
-            a+=S[j];
-        }
-        is_exist[a]++;
+        is_exist[S.substr(i,L)]++;
+        //cout << S.substr(i,L) << endl;
     }
-    
-    //檢查最小字典
-    int loc[L.length()]={0},now=0,last=0;
-    while(true){
-        if(now<)
-    }
+
+    //檢查最小字典(DFS)
+    ans=string(L,S[0]);
+    DFS(0);
+
 return 0;
 }
