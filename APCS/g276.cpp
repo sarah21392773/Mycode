0c463646f285b78e3a1c418fa67e3c1ab31b4513
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-07-07 16:29:29 +0800

    g276. 2. 魔王迷宮

--- APCS/g276.cpp
+++ APCS/g276.cpp
@@ -1,34 +1,74 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,m,k;
+struct TheBoss {
+    int r, c, s, t;
+    int now_x, now_y;
+    int is_exist = 1; // 1表示存在, 0表示消失
+};
+
+int main() {
+    int n, m, k;
     cin >> n >> m >> k;
-    int filed[n+2][m+2];
-    for(int i=0;i<n+2;i++){
-        for(int j=0;j<m+2;j++){
-            filed[i][j]=0;      //0是空地
+    vector<vector<int>> bomb(n, vector<int>(m, 0));
+    vector<TheBoss> boss(k);
+    for (int i = 0; i < k; i++) {
+        cin >> boss[i].r >> boss[i].c >> boss[i].s >> boss[i].t;
+        boss[i].now_x = boss[i].r;
+        boss[i].now_y = boss[i].c;
+    }
+
+    while (true) {
+        bool has_boss = false;
+        for (int i = 0; i < k; i++) {
+            if (boss[i].is_exist == 1) {
+                has_boss = true;
+                // 在當前位置放炸彈
+                bomb[boss[i].now_x][boss[i].now_y]++;
+            }
+        }
+
+        if (!has_boss) break;
+
+        for (int i = 0; i < k; i++) {
+            if (boss[i].is_exist == 1) {
+                // 移動魔王
+                boss[i].now_x += boss[i].s;
+                boss[i].now_y += boss[i].t;
+
+                // 檢查是否超出邊界
+                if (boss[i].now_x < 0 || boss[i].now_x >= n || boss[i].now_y < 0 || boss[i].now_y >= m) {
+                    boss[i].is_exist = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (boss[i].is_exist == 1) {
+                // 檢查是否踩到炸彈
+                if (bomb[boss[i].now_x][boss[i].now_y] > 0) {
+                    // 引爆炸彈，移除炸彈
+                    bomb[boss[i].now_x][boss[i].now_y] = 0;
+                    boss[i].is_exist = 0;
+
+                    // 檢查是否有其他魔王在同一位置
+                    for (int j = 0; j < k; j++) {
+                        if (i != j && boss[j].now_x == boss[i].now_x && boss[j].now_y == boss[i].now_y && boss[j].is_exist == 1) {
+                            boss[j].is_exist = 0;
+                        }
+                    }
+                }
+            }
         }
     }
-    int r[k],c[k],s[k],t[k];
-    for(int i=0;i<k;i++){
-        cin >> r[i] >> c[i] >> s[i] >> t[i];
-        filed[r[i]+1][c[i]+1]=8;      //8是魔王
-    }
-/*
-    while(true){
-        for(int i=0;i<k;i++){
-            filed[]
+
+    int remaining_bombs = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (bomb[i][j] > 0) remaining_bombs++;
         }
     }
-*/
-//
-    for(int i=0;i<n+2;i++){
-        for(int j=0;j<m+2;j++){
-            cout << filed[i][j] << " ";      //0是空地
-        }
-        cout << endl;
-    }
-//
-return 0;
+
+    cout << remaining_bombs << endl;
+    return 0;
 }
