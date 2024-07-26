c79917a59a55b36a52eb4e413bc47d899eea6352
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-07-26 22:36:28 +0800

    Update k253.cpp

--- toi/k253.cpp
+++ toi/k253.cpp
@@ -4,29 +4,49 @@ using namespace std;
 int main(){
     int T,S,P;
     cin >> T >> S >> P;
-    int ans=0,buy=0,many=T-buy;
-    while(many>=T*0.2 && buy<=S){
+    int ans=0,many=T;
+    while(many>=T*0.2-1&&S>0){
         int tem=0;
-        many=T-buy;
-        if (S-buy<=many){
-            tem=int(T*0.2)*P;
-            buy+=int(T*0.2);
-        }
-        else{
-            tem=many*P;
-            buy+=many;
-        }
         if (many>=T*0.8){
-            ans+=int(tem*0.5);
+            tem=int(P*0.5);
         }
         else if(many>=T*0.6){
-            ans+=int(tem*0.6);
+            tem=int(P*0.6);
         }
         else if(many>=T*0.4){
-            ans+=int(tem*0.8);
+            tem=int(P*0.8);
         }
         else{
-            ans+=int(tem*0.9);
+            tem=int(P*0.9);
+        }
+        if (S<=T*0.2){
+            if (S<=many){
+                ans+=S*tem;
+                many-=S;
+                S-=S;
+            }
+            else{
+                ans+=many*tem;
+                S-=many;
+                many-=many;
+            }
+        }
+        else{
+            if (T*0.2<=many){
+                ans+=T*0.2*tem;
+                many-=(T*0.2);
+                S-=(T*0.2);
+            }
+            else{
+                ans+=many*tem;
+                S-=many;
+                many-=many;
+            }
+        }
+        if (many>=T*0.8-1&&S>=T*0.2+1){
+            ans+=tem;
+            S-=1;
+            many-=1;
         }
     }
     cout << ans;
