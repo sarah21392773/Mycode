37f75b42089e1c01a9b2f4e2ddb844df7cb7fa2b
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-08-19 00:23:40 +0800

    矩陣翻轉

--- APCS/b965.cpp
+++ APCS/b965.cpp
@@ -1,116 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int R,C,M;
+vector<vector<int>> v(25,vector<int>(25));
+
+void ShanJuan(int r,int c){
+    vector<vector<int>> temp(25,vector<int>(25));
+    for(int i=1;i<=r;i++)
+        for(int j=1;j<=c;j++)
+            temp[c-j+1][i]=v[i][j];
+    v.clear();
+    v=temp;
+    swap(c,r);
+    R=r,C=c;
+}
+
+void FunJuan(int R,int C){
+    vector<vector<int>> temp(25,vector<int>(25));
+    for(int i=1;i<=R;i++)
+        for(int j=1;j<=C;j++)
+            temp[R-i+1][j]=v[i][j];
+    v.clear();
+    v=temp;
+}
+
 int main(){
-    int R,C,M;
     cin >> R >> C >> M;
-    int a[R][C],b[C][R];
-    char rect='a';
-    for(int i=0;i<R;i++){
-        for(int j=0;j<C;j++){
-            cin >> a[i][j];
-            b[j][i]=0;
-        }
-    }
+    int way[M+5]={0};
+    for(int i=1;i<=R;i++)
+        for(int j=1;j<=C;j++) cin >> v[i][j];
+    for(int i=0;i<M;i++) cin >> way[i];
     for(int i=0;i<M;i++){
-        int m;
-        cin >> m;
-        //旋轉 0
-        if(m==0){
-            if(rect=='a'){
-                for(int i=0;i<R;i++){
-                    for(int j=0;j<C;j++){
-                        b[j][R-i-1]=a[i][j];
-                    }
-                }
-                rect='b';
-            }
-            else{
-                for(int i=0;i<C;i++){
-                    for(int j=0;j<R;j++){
-                        a[j][C-i-1]=b[i][j];
-                    }
-                }
-                rect='a';
-            }
-        }
-        //翻轉 1
-        else{
-            if(rect=='a'){
-                int tem[R][C];
-                for(int i=0;i<R;i++){
-                    for(int j=0;j<C;j++){
-                        tem[i][j]=a[R-1-i][j];
-                    }
-                }
-                for(int i=0;i<R;i++){
-                    for(int j=0;j<C;j++){
-                        a[i][j]=tem[i][j];
-                    }
-                }
-            }
-            else{
-                int tem[C][R];
-                for(int i=0;i<C;i++){
-                    for(int j=0;j<R;j++){
-                        tem[i][j]=b[C-1-i][j];
-                    }
-                }
-                for(int i=0;i<C;i++){
-                    for(int j=0;j<R;j++){
-                        b[i][j]=tem[i][j];
-                    }
-                }
-            }
-        }
-        /*
-        //輸出
-        if(rect=='a'){
-            cout << "a" << endl;
-            for(int i=0;i<R;i++){
-                for(int j=0;j<C;j++){
-                    cout << a[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
-        else{
-            cout << "b" << endl;
-            for(int i=0;i<C;i++){
-                for(int j=0;j<R;j++){
-                    cout << b[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
-        cout << endl;
-        //
-    */
+        if(way[M-i-1]==0) ShanJuan(R,C);
+        else FunJuan(R,C);
     }
-    //輸出
-    if(rect=='a'){
-        cout << R << " " << C << endl;
-        for(int i=0;i<R;i++){
-            for(int j=0;j<C;j++){
-                cout << a[i][j];
-                if(j!=C-1){
-                    cout << " ";
-                }
-            }
-            cout << endl;
-        }
-    }
-    else{
-        cout << C << " " << R << endl;
-        for(int i=0;i<C;i++){
-            for(int j=0;j<R;j++){
-                cout << b[i][j];
-                if(j!=R-1){
-                    cout << " ";
-                }
-            }
-            cout << endl;
-        }
-    }
-return 0;
+    cout << R << " " << C << endl;
+    for(int i=1;i<=R;i++)
+        for(int j=1;j<=C;j++)
+            cout << v[i][j] << " \n"[j==C];
+    return 0;
 }
