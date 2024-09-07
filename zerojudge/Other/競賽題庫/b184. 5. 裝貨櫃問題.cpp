b1dcd0aec804e3298d9260ebab02f28a124929c6
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-09-07 11:03:20 +0800

    Create b184. 5. 裝貨櫃問題.cpp

---
+++ zerojudge/Other/競賽題庫/b184. 5. 裝貨櫃問題.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 105;
+const int MAXW = 105;
+int w[MAXN], p[MAXN];
+int K[MAXN][MAXW];
+
+int main(){
+
+	int n;
+	while(cin >> n){
+
+		for(int i = 1; i <= n; i++)
+			cin >> w[i] >> p[i];
+
+
+		for(int i = 0; i <= n ; i++){
+			for(int j = 0; j <= 100 ; j++){
+				if(i == 0 || j == 0)  K[i][j] = 0 ;
+				else if(w[i] > j)  K[i][j] = K[i-1][j];
+				else  K[i][j] = max(p[i]+K[i-1][j-w[i]],K[i-1][j]);
+			}
+		}
+		cout << K[n][100] << endl;
+	}
+
+return 0;
+}
