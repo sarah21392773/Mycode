62a22b817e5606163230a9a225c75512d78580fa
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-09-07 11:05:42 +0800

    Create c833. 第四題：二維陣列.cpp

---
+++ zerojudge/Other/競賽題庫/c833. 第四題：二維陣列.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int N,M;
+    cin >> N >> M;
+    vector<vector<char>>filed;
+    map <int,bool> x,y;
+    for(int i=0;i<N;i++){
+        vector<char> row;
+        for(int j=0;j<M;j++){
+            char a;
+            cin >> a;
+            if(a=='#'){
+                x[j]=true;
+                y[i]=true;
+            }
+            row.push_back(a);
+        }
+        filed.push_back(row);
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(x[j]==true||y[i]==true){
+                cout << '#';
+            }
+            else{
+                cout << 'X';
+            }
+        }
+        cout << endl;
+    }
+return 0;
+}
