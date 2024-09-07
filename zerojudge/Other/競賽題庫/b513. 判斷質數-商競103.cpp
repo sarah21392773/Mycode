8eca85eccebb3b339bd8c8114b996ab0da9891f7
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-09-07 11:41:00 +0800

    Update b513. 判斷質數-商競103.cpp

--- zerojudge/Other/競賽題庫/b513. 判斷質數-商競103.cpp
+++ zerojudge/Other/競賽題庫/b513. 判斷質數-商競103.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     int n=0;
-    cin >> n;
+    cin >> n;        //n是要測試的數量
     for(int i=0;i<n;i++){
-        int x=0,j=2;
+        int x=0,j=2;    //x是要判斷的數字，j是因數
         bool a=true;
         cin >> x;
-        while( j*j <= x){
-            if(x%j==0){
+        while( j*j <= x){    //當j<=根號x時，繼續判斷
+            if(x%j==0){    //若x可以被j整除，就代表x不是質數
                 a=false;
                 break;
             }
