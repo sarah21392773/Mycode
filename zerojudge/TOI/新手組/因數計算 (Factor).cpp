deb6c0b7714c994b2e8cfe39650b36d47013fa33
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-10-01 11:30:58 +0800

    Update 因數計算 (Factor).cpp

--- zerojudge/TOI/新手組/因數計算 (Factor).cpp
+++ zerojudge/TOI/新手組/因數計算 (Factor).cpp
@@ -8,10 +8,12 @@ signed main(){
     ios::sync_with_stdio(false);cin.tie(0);
     cin >> x >> y;
     for(int i=x;i<=y;i++){
-        int Tm=1;
-        for(int j=1;j<i;j++){
+        int Tm=0,stop=sqrt(i);
+        for(int j=1;j<=stop;j++){
             if(i%j==0) Tm++;
         }
+        Tm*=2;
+        if(stop*stop == i) Tm--;
         if(Tm>M){
             M=Tm;
             N=i;
