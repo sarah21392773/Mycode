2681e71e813a573e53967c48b99bba67c657067d
sarah21392773 <162683090+sarah21392773@users.noreply.github.com>
2024-10-02 21:21:51 +0800

    Update k463. 破冰活動 (Icebreaker).cpp

--- zerojudge/TOI/潛力組/k463. 破冰活動 (Icebreaker).cpp
+++ zerojudge/TOI/潛力組/k463. 破冰活動 (Icebreaker).cpp
@@ -2,24 +2,27 @@
 #define int long long
 using namespace std;
 
-int N,M,group[2],temp;
+int N,M,group[2],ans;
 vector<int> graph[10005];
 int vis[10005];
 bool is_vaild=true;
 
 void DFS(int n){
+    //走訪相鄰節點
     for(auto &i:graph[n]){
+        //已經被走訪過
         if(vis[i]){
+            //兩相鄰節點同色 => 不合法
             if(vis[i]==vis[n]){
                 is_vaild=false;
                 cout << "0\n";
                 exit(0);
             }
+            //不同色 => 繼續走訪其他相鄰節點
             continue;
         }
-        temp++;
-        vis[i]=3-vis[n];
-        group[vis[i]-1]++;
+        vis[i]=3-vis[n];    //染色
+        group[vis[i]-1]++;  //更新小組人數
         DFS(i);
     }
 }
@@ -27,20 +30,25 @@ void DFS(int n){
 signed main(){
     ios::sync_with_stdio(false);cin.tie(0);
     cin >> N >> M;
+
+    //建圖
     int A,B;
     while(M--){
         cin >> A >> B;
         graph[A].push_back(B);
         graph[B].push_back(A);
     }
+
+    //枚舉
     for(int i=1;i<=N;i++){
         if(vis[i]) continue;
+        group[0]=group[1]=0;
         vis[i]=1;
         group[0]++;
-        if(!graph[i].empty()) temp++;
         DFS(i);
+        //把人數多的都放到同一組
+        ans+=max(group[0],group[1]);
     }
-    int ans=max(group[0],group[1])+(N-temp);
     cout << ans << "\n";
 return 0;
 }
