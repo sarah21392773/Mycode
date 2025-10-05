f8d6f280a8b2d4cf3a96cf467620e059d1e7c88f
Weee <162683090+sarah21392773@users.noreply.github.com>
2025-10-05 09:41:03 +0800

    Create p903. 萬有引力 (Gravitation).cpp

---
+++ zerojudge/TOI/新手組/p903. 萬有引力 (Gravitation).cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int m1, m2, r;
+
+int main(){
+ios::sync_with_stdio(false);
+    cin >> m1 >> m2 >> r;
+    cout << (6*m1*m2)/(r*r) << "\n";
+return 0;
+}
