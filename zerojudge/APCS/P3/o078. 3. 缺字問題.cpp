42ce6075c357c560599c9ef22e11d762bf729f8b
Weee <162683090+sarah21392773@users.noreply.github.com>
2025-11-14 23:03:55 +0800

    Update o078. 3. 缺字問題.cpp

--- zerojudge/APCS/P3/o078. 3. 缺字問題.cpp
+++ zerojudge/APCS/P3/o078. 3. 缺字問題.cpp
@@ -1,43 +1,47 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
-string K,S,ans;
-int L;
-map<string,int> is_exist;
+string K, S;
+vector<char> temp;
+bool is_find;
+int L, lenS;
+map<string, int> elements;
 
-void DFS(int idx){
-    //如果ans長度達L
-    if(idx>=L){
-        //如果出現在is_exist中，就不是答案
-        if(is_exist[ans]>=1) return;
-        //否則是答案
-        cout << ans << endl;
-        exit(0);
+void F(int index){
+    for(int j=0; j<K.size(); j++){
+        if(is_find == true) return;
+        if(index<L){
+            temp.push_back(K[j]);
+            F(index+1);
+        }
+        else{
+            string st = "";
+            for(auto &w: temp) st += w;
+            if(elements[st] == 0){
+                cout << st << "\n";
+                is_find = true;
+                j = K.size();
+            }
+            temp.pop_back();
+            index --;
+            return;
+        }
     }
-    if (idx >= ans.size()) return;
-    //否則ans長度未達L，就繼續枚舉
-    for(int i=0;i<S.size();i++){
-        ans[idx] = S[i];
-        // 繼續 DFS，因為選了一個字母所以 idx 和 cnt 都要 + 1
-        DFS(idx+1);
-    }
+    temp.pop_back();
+    index --;
+    return;
 }
 
-int main(){
-
-    cin.sync_with_stdio(0);
-    cin.tie(0);
+signed main(){
+ios::sync_with_stdio(false);cin.tie(0);
     cin >> K >> L >> S;
-
-    //看S中出現字串
-    for(int i=0;i<S.length()-L+1;i++){
-        is_exist[S.substr(i,L)]++;
-        //cout << S.substr(i,L) << endl;
+    lenS = S.size();
+    for(int i=0; i<=lenS-L; i++){
+        string ele = "";
+        for(int j=i; j<i+L; j++) ele += S[j];
+        elements[ele]++;
     }
-
-    //檢查最小字典(DFS)
-    ans=string(L,S[0]);
-    DFS(0);
-
+    F(0);
 return 0;
 }
